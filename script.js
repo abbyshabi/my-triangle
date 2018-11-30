66e712390344c021a9926d4e4cbd8d3efb57d485
abby-shabi <abbyshabi@gmail.com>
2018-11-30 21:07:53 +0300

    made new changes to my file

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function getResults() {
 	var c = parseInt(document.getElementById("cValue").value);
 
 	if(a == b && b == c) {
-		document.getElementById('answer').value = 'Equilateral'
+		document.getElementById('answer').value = 'An Equilateral: An equilateral triangle is a triangle with three equal sides'
 	}
 	else if (a == b || a == c || b == c){
-		document.getElementById('answer').value = 'Isoceles'
+		document.getElementById('answer').value = 'An Isoceles : An Isoceles triangle is a triangle with two equal sides and the third side is different'
 	}
 	else if (a != b != c) {
 		//console.log(a+b)
@@ -16,7 +16,7 @@ function getResults() {
 		document.getElementById('answer').value = 'Not a Triangle'
 	} 
 	else {
-		document.getElementById('answer').value = 'Scalene'
+		document.getElementById('answer').value = 'A Scalene : A scalene triangle doesn\'t have any equal side, all the sides are different from one another'
 	}
 }
 	
